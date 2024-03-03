4121fd16ac95d145e7281d9c4fb3a12ae04d118c
Nick Halvorsen <nick@hugemail.win>
2024-03-02 20:43:33 -0500

    miryoku rules.mk

---
+++ keyboards/splitkb/kyria/keymaps/manna-harbour_miryoku/rules.mk
@@ -0,0 +1,4 @@
+OLED_DRIVER_ENABLE = no   # Enables the use of OLED displays
+# needed to disable to get under file size limit
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
