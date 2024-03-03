37be9a2795583e7485e3b55cf7a37ac334ab1d03
Nick Halvorsen <nick@hugemail.win>
2024-03-02 21:58:02 -0500

    more fixes

--- keyboards/kbdfans/kbd67/mkiirgb/keymaps/nickhalvorsen/rules.mk
+++ keyboards/kbdfans/kbd67/mkiirgb/keymaps/nickhalvorsen/rules.mk
@@ -1,2 +1,7 @@
 
-#RGB_MATRIX_ENABLE = no        # Use RGB matrix
+# saves 6kb if disabled
+# RGB_MATRIX_ENABLE = no        # Use RGB matrix
+
+# compiler time optimizations to decrease file size
+# saves 3kb
+EXTRAFLAGS+=-flto
